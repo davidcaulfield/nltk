d5c17eabfa237ce0f76ff2ce7151dfeff50c9417
David <dcaulfie@stetson.edu>
2017-04-11 17:29:55 -0400

    added tokenize functions

---
+++ np_processing.py
@@ -0,0 +1,6 @@
+from nltk.tokenize import word_tokenize, sent_tokenize
+
+
+
+example_text = 'Hi there, how are you doing today. Python is awesome. The sky is blue.'
+print(sent_tokenize(example_text))
